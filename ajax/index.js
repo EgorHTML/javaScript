36331d76054e7ccee03f67aa53758ec2c3a1adc5
egor <kerminegor@gmail.com>
2021-03-02 20:29:57 +0300

    ajax

--- ajax/index.js
+++ ajax/index.js
@@ -1,17 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-    <title>Document</title>
-</head>
-<body>
-    <div class="container"></div>
-    <script>
-        const xhr =new XMLHttpRequest()
+const xhr =new XMLHttpRequest()
 const container = document.querySelector('.container')
 container.style.cursor = 'pointer'
 xhr.open('get','https://jsonplaceholder.typicode.com/users')
@@ -49,6 +36,3 @@ function renderUsers(){
         }
     })
 }
-    </script>
-</body>
-</html>
